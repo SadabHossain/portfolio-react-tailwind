b80b3b3a210b3b8cd436c416511e9db0e07e25f0
self <sadabhossain_pramanik@yahoo.in>
2025-06-01 22:22:51 +0530

    Portfolio commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# portfolio-react-tailwind
-💼 A responsive and modern portfolio web application built with React.js and Tailwind CSS.
+# Build a Modern Portfolio Website with React & TailwindCSS
+
+<div align="center">
+ 
+## ⚙️ Tech Stack
+
+* **React** – Component-based UI development
+* **Vite** – Lightning-fast build tool
+* **TailwindCSS** – Utility-first CSS for styling
+* **Lucide Icons** – Clean and beautiful icon pack
+* **Radix UI** – Accessible component primitives
+* **TypeScript (optional)** – Type safety and tooling
+* **GitHub & Vercel** – Deployment
+
+
+
+### Clone and Run Code base 
+
+```bash
+git clone https://github.com/SadabHossain/portfolio-react-tailwind.git
+cd portfolio-react-tailwind.git
+npm install
+npm run dev
+```
+
+Your app will be available at: [http://localhost:5173](http://localhost:5173)
+
+
+## ☁️ Deployment
+
+### Deploy on Vercel
+
+1. Push your code to GitHub
+2. Go to [vercel.com](https://vercel.com)
+3. Import your repository
+4. Click **Deploy**
+
+Your live website will be hosted on a custom subdomain (e.g. `https://your-name.vercel.app`)
+
+---
+
+## 🔗 Useful Links
+
+* [React Documentation](https://reactjs.org/)
+* [Tailwind CSS Docs](https://tailwindcss.com/)
+* [Lucide Icons](https://lucide.dev/)
+* [Radix UI](https://www.radix-ui.com/)
+* [Vite](https://vitejs.dev/)
+* [Vercel](https://vercel.com/)
+
+---
---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,198 @@
+import {
+  Instagram,
+  Linkedin,
+  Mail,
+  MapPin,
+  Phone,
+  Send,
+  Twitch,
+  Twitter,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import data from "../data/portfolio.json";
+import image from "../assets/Sadab_Photo_HD.png"
+
+export const ContactSection = () => {
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { email, mobile, address } = data;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+
+    setTimeout(() => {
+      toast({
+        title: "Message sent!",
+        description: "Thank you for your message. I'll get back to you soon.",
+      });
+      setIsSubmitting(false);
+    }, 1500);
+  };
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Get In <span className="text-primary"> Touch</span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Have a project in mind or want to collaborate? Feel free to reach out.
+          I'm always open to discussing new opportunities.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">
+              {" "}
+              Contact Information
+            </h3>
+
+            <div className="space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Email</h4>
+                  <a
+                    href="sadabhossain_pramanik@yahoo.in"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    {email}
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Phone</h4>
+                  <a
+                    href="Mobile:+919932195224"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    {mobile}
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <MapPin className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    {address}
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a href="#" target="_blank">
+                  <Linkedin />
+                </a>
+                <a href="#" target="_blank">
+                  <Twitter />
+                </a>
+                <a href="#" target="_blank">
+                  <Linkedin />
+                </a>
+                <a href="#" target="_blank">
+                  <Twitch />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="p-8 rounded-lg shadow-xs"
+          >
+            <div className="h-98 overflow-hidden">
+              <img
+                src={image}
+                alt="Image"
+                className="w-full h-full object-cover transition-transform duration-600 group-hover:scale-200"
+              />
+            </div>
+          </div>
+          {/* <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3> */}
+
+          {/* <form className="space-y-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="Pedro Machado..."
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="john@gmail.com"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
+                  placeholder="Hello, I'd like to talk about..."
+                />
+              </div>
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={cn(
+                  "cosmic-button w-full flex items-center justify-center gap-2"
+                )}
+              >
+                {isSubmitting ? "Sending..." : "Send Message"}
+                <Send size={16} />
+              </button>
+            </form> */}
+          {/* </div> */}
+        </div>
+      </div>
+    </section>
+  );
+};
